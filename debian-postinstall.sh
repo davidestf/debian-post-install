f0d5fed1503a691e0dd7e3b78c55f3b10fce1507
Davide staffieri <davide@protoweb.me>
2023-01-24 13:13:46 +0000

    Update debian-postinstall.sh

--- debian-postinstall.sh
+++ debian-postinstall.sh
@@ -10,7 +10,7 @@ sudo apt install gnupg2
 echo "Adding repositoy"
 
 #Sublime
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+curl -sS https://download.sublimetext.com/sublimehq-pub.gpg |  gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublime.gpg
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
 #Insomnia
